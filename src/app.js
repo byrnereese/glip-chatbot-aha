2ed52998f542f259545b119c3aa2e10dcda8b1bf
byrnereese <byrne@majordojo.com>
2019-03-13 09:53:25 -0700

    Added ability to subscribe to webhook notifications and notify a team of changes in Aha

--- src/app.js
+++ src/app.js
@@ -4,8 +4,12 @@ import { Service, Bot } from 'ringcentral-chatbot/dist/models'
 import { handle } from './bot'
 import { ahaOAuth } from './aha'
 import { sendAuthorizationLink } from './util'
+import { AllHtmlEntities } from 'html-entities'
+import turnDownService from 'turndown'
 
 const app = createApp(handle)
+const entities = new AllHtmlEntities()
+const turnDown = new turnDownService()
 
 app.get('/aha/oauth', async (req, res) => {
     const { code, state } = req.query
@@ -33,6 +37,79 @@ app.get('/aha/oauth', async (req, res) => {
     await bot.sendMessage(groupId, { text: `I have been authorized to fetch data from Aha` })
     res.send('<!doctype><html><body><script>close()</script></body></html>')
 })
+
+app.post('/aha/webhook', async (req, res) => {
+    const { groupId, botId } = req.query
+    let audit = req.body.audit
+    console.log(`Received webhook from Aha (group: ${groupId}, bot: ${botId})...`)
+    console.log("Changes: ", audit.changes )
+    const bot = await Bot.findByPk(botId)
+    let changes = []
+    let seen_fields = []
+    for (var i in audit.changes) {
+	let change = audit.changes[i]
+	console.log("Adding change to list: ", change)
+	let ignore_fields = new RegExp('(Created by user|Rank|Assigned to user|Show feature remaining estimate|Reference num)')
+	if (change.value == '' || // empty value
+	    (ignore_fields.test(change.field_name) && audit.audit_action === "create") || // field to ignore
+	    seen_fields.includes(change.field_name) // duplicate field
+	   ) {
+	    continue
+	}
+	let shortDesc = "Short"
+	switch(change.field_name) {
+	case "Name":
+	case "Description":
+	    shortDesc = "Long"
+	    break
+	}
+	let change_value = ''
+	if (audit.auditable_type === "note") {
+	    change_value = turnDown.turndown(change.value.toString())
+	} else {
+	    change_value = entities.decode(change.value.toString())
+	}
+	let change_instruction = {
+		"title": change.field_name,
+		"value": change_value,
+	    "style": shortDesc
+	}
+	if (change.field_name === "Name") {
+	    changes.splice( 0, 0, change_instruction )
+	} else {
+	    changes.push( change_instruction )
+	}
+	seen_fields.push( change.field_name )
+    }
+    console.log( req.body )
+    if (audit.interesting) { 
+	await bot.sendMessage(groupId, {
+	    "text": `${req.body.audit.user.name} ${req.body.audit.description}`,
+	    "attachments": [
+		{
+		    "type": "Card",
+		    "color": "#00FF2A",
+		    //	"fallback": "Some fallback text",
+		    //	"intro": "The following fields were modified in Aha:",
+		    //	"author": {
+		    //	    "name": "Byrne Reese"
+		    //	    ,"uri": "https://example.com/author_link"
+		    //	    ,"iconUri": "https://example.com/author_icon.png"
+		    //	},
+		    "title": `Aha ${audit.audit_action}`,
+		    "text": `The following fields were modified ${audit.auditable_url}`,
+		    "fields": changes,
+		    "footnote": {
+			"text": `Changes made by ${audit.user.name}`,
+			"time": audit.created_at
+		    }
+		}
+	    ]
+	})
+    }
+    res.send('<!doctype><html><body>OK</body></html>')
+})
+
 
 /*
 app.put('/aha/refresh-tokens', async (req, res) => {
--- src/bot.js
+++ src/bot.js
@@ -21,19 +21,42 @@ const handleMessage4Bot = async event => {
     const service = await Service.findOne({ where: {
 	name: 'Aha', botId: bot.id, groupId: group.id
     } })
-    if (text === 'ping') {
-	await bot.sendMessage(group.id, { text: 'pong' })
-    } else if (text === 'bind') {
-	let ahaAuthUrl = ahaOAuth.authorizeUri( process.env.RINGCENTRAL_CHATBOT_SERVER + '/aha/oauth', { responseType: 'code', state: `${group.id}:${bot.id}:${userId}` } )
-	await bot.sendMessage(group.id, { text: `Let's [authorize me](${ahaAuthUrl}).` })
-    } else if (text.startsWith("feature")) {
-	let found = text.match(/feature (.*)$/)
-	let featureId = found[1]
-	let token = service.data.token
-	let aha = getAhaClient( token )
-	let resp = aha.feature.get( featureId, function( err, data, response ) {
-	    bot.sendMessage(group.id, { text: `Feature you queried: ${data.feature.name}` })
-	});
+    let token = service.data.token
+
+    if (text === 'bind') {
+	if (token) {
+	    await bot.sendMessage(group.id, { text: `It appears you already have an active connection to Aha in this team.` })
+	} else {
+	    let ahaAuthUrl = ahaOAuth.authorizeUri( process.env.RINGCENTRAL_CHATBOT_SERVER + '/aha/oauth', { responseType: 'code', state: `${group.id}:${bot.id}:${userId}` } )
+	    await bot.sendMessage(group.id, { text: `Let's [authorize me](${ahaAuthUrl}).` })
+	}
+
+    } else if (text === 'unbind') {
+	if (token) {
+	    // TODO - remove all tokens and subscriptions
+	    await bot.sendMessage(group.id, { text: `TODO: remove all tokens and subscriptions` })
+	} else {
+	    await bot.sendMessage(group.id, { text: `It does not appear you have a current connection to Aha in this team.` })
+	}
+	
+    } else if (text.startsWith("subscribe")) {
+	// TODO - persist in database that this group is subscribed to a product id
+	if (token) {
+	    let found = text.match(/subscribe (.*)$/)
+	    let productCode = found[1]
+	    let aha = getAhaClient( token )
+	    let server = process.env.RINGCENTRAL_CHATBOT_SERVER
+	    let hookUrl = server + `/aha/webhook?group=${group.id}&bot=${bot.id}`
+	    let resp = aha.product.get( productCode, function( err, data, response ) {
+		bot.sendMessage(group.id, { text: `To receive updates in this Team from Aha:\n1. [Create a new Activity Webhook in Aha](https://ringcentral.aha.io/settings/projects/${found}/integrations/new)\n2. In the Hook URL field, enter: ${hookUrl}\n3. Select the activities you would like to subscribe to.` })
+	    });
+	} else {
+	    await bot.sendMessage(group.id, { text: `It does not appear you have a current connection to Aha in this team.` })
+	}
+
+    } else if (text === "help") {
+	await bot.sendMessage(group.id, { text: `Here are the commands I am able to respond to:\n* **bind** - connect to your Aha account\n* **unbind** - disconnect from your Aha account\n* **subscribe <product ID>** - pass in the three letter product id and get directions on how to start receiving notifications for changes in that product` })
+
     }
 }
 
