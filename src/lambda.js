950c34cf47a758708542fdcf2b92c374aae22003
byrnereese <byrne@majordojo.com>
2019-03-25 09:41:22 -0700

    Fixed broken deploy due to bad dep

--- src/lambda.js
+++ src/lambda.js
@@ -1,7 +1,7 @@
 const createApp = require('ringcentral-chatbot/dist/apps').default
 const { createAsyncProxy } = require('ringcentral-chatbot/dist/lambda')
 const serverlessHTTP = require('serverless-http')
-import { handle } from './bot'
+const handle = require('./bot')
 
 const app = createApp(handle)
 module.exports.app = serverlessHTTP(app)
