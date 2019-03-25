a25634c91880e607bb38ca1dd93b32b9561942f2
byrnereese <byrne@majordojo.com>
2019-03-25 10:01:01 -0700

    Fixing lambda

--- src/lambda.js
+++ src/lambda.js
@@ -1,8 +1,11 @@
-const createApp = require('ringcentral-chatbot/dist/apps').default
-const { createAsyncProxy } = require('ringcentral-chatbot/dist/lambda')
 const serverlessHTTP = require('serverless-http')
-const handle = require('./bot')
+const { createAsyncProxy } = require('ringcentral-chatbot/dist/lambda')
+const createApp = require('ringcentral-chatbot/dist/apps').default
 
-const app = createApp(handle)
+const app = require('./app').default
+//import app from './app'
+//const handle = require('./bot')
+//const app = createApp(handle)
+
 module.exports.app = serverlessHTTP(app)
 module.exports.proxy = createAsyncProxy('app')
