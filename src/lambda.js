043e20726bb2c6ebaf72a11be311c06f60fe06b3
byrnereese <byrne@majordojo.com>
2019-03-25 13:19:10 -0700

    Removing npm package lock in favor of yarn

--- src/lambda.js
+++ src/lambda.js
@@ -1,9 +1,8 @@
 const serverlessHTTP = require('serverless-http')
 const { createAsyncProxy } = require('ringcentral-chatbot/dist/lambda')
 const createApp = require('ringcentral-chatbot/dist/apps').default
-
 const app = require('./app').default
-//import app from './app'
+
 //const handle = require('./bot')
 //const app = createApp(handle)
 
